3c54ace43734aafb48ce2220497a29b888787913
angiecm <angelik2115@yahoo.es>
2018-02-17 18:16:42 +0100

    arreglandoSettings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+sqbpywaa^3mkl03wgd80k#)=!o8)!lgfpgsvk28sovty-jtjs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -101,3 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path(BASE_DIR,'static')
